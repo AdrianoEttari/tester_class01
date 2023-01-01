765fdad85fbeb323c9fc9121b343988a3dae2872
Adriano Ettari <adrianoettari@MacBook-Air-di-Adriano.local>
2023-01-01 13:47:46 +0100

    Add animal.py

---
+++ animal.py
@@ -0,0 +1,21 @@
+import sys
+
+def cat():
+    print('Meow')
+
+def default():
+    print('Hello')
+
+def dog():
+    print('Woof!')
+
+def main():
+    if sys.argv[1]=='cat':
+        cat()
+    elif sys.argv[1]=='dog':
+        dog()
+    else:
+        default()
+
+if __name__ == '__main__':
+	main()
